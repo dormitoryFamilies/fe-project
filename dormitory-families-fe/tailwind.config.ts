0dded2f73fb84e9242359cac5c6c76bfb959f9fc
rusia9217 <110519493+unnimm@users.noreply.github.com>
2024-01-02 02:39:47 +0900

    feat: tailwind css 초기세팅
    tailwind css 초기 세팅
    스타일 시스템 세팅 (fontsize, color)

--- dormitory-families-fe/tailwind.config.ts
+++ dormitory-families-fe/tailwind.config.ts
@@ -7,12 +7,65 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      //화면 사이즈
+      md: '375px',
+    },
+    colors: {
+      // Brand colors
+      primary: "#E70050",
+      second: "#FFAEB8",
+
+      // Particular colors
+      point: "#FF5449",
+
+      // Achromatic colors
+      black: "#191919",
+      gray4: "#4F4F4F",
+      gray3: "#767676",
+      gray2: "#999999",
+      gray1: "#DBDBDB",
+      gray0: "#F4F4F4",
+      white: "#FFFFFF",
+
+      // 그 외 추가 컬러
+      // ...
+    },
+    fontSize: {
+      //헤더 사이즈
+      header1: "64px",
+
+      //타이틀 사이즈
+      title1: "32px", // 대 타이틀
+      title2: "24px", // 중 타이틀
+      title3: "20px", // 소 타이틀 2
+      title4: "18px", // 소 타이틀
+      title5: "16px", // 본문 2
+
+      //서브타이틀 사이즈
+      subtitle1: "15px", // 본문
+      subtitle2: "13px", // 작은
+
+      //body 사이즈
+      body1: "16px",
+      body2: "14px",
+
+      //button 사이즈
+      button: "14px"
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      spacing: {
+        main: "1920px",
+        home: "1434px",
+        home2: "940px",
+        side: "486px",
       },
+    },
+    minWidth: {
+      main: "1920px",
+      home: "1434px",
+      home2: "940px",
+      side: "486px",
     },
   },
   plugins: [],
