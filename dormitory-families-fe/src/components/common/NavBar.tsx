eb09d871659c3a062a44e1535c4a838ec9742771
rusia9217 <110519493+unnimm@users.noreply.github.com>
2024-01-12 15:28:27 +0900

    feat: NavBar 구현
    네비게이션 바를 구현하였습니다.

--- dormitory-families-fe/src/components/common/NavBar.tsx
+++ dormitory-families-fe/src/components/common/NavBar.tsx
@@ -5,13 +5,13 @@ import { MENU_LIST } from '@/utils/nav';
 
 export default function NavBar() {
     return (
-        <div className="flex justify-center items-center">
-            <div className="fixed bottom-0 flex justify-evenly items-center gap-[0.8rem] pb-[0.5rem]">
+        <div className="flex justify-center items-center ">
+            <div className="fixed bottom-0 bg-white flex rounded-t-[32px] py-3 px-5 justify-evenly gap-x-[30px] items-center drop-shadow-[0_-4px_12px_rgba(0,0,0,0.08)]">
                 {MENU_LIST.map((menu) => {
                     return (
-                        <Link href={menu.path} key={menu.id} className="w-[4rem] flex flex-col justify-center items-center">
+                        <Link href={menu.path} key={menu.id} className="h-[52px] text-[12px] flex flex-col justify-center items-center">
                             <menu.Icon />
-                            <span>{menu.name}</span>
+                            <span className="text-h5 text-gray4">{menu.name}</span>
                         </Link>
                     );
                 })}
