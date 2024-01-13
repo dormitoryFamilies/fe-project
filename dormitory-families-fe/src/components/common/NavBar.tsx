78efb7690efe8d3a7354aaaba7dc38b09af905c7
rusia9217 <110519493+unnimm@users.noreply.github.com>
2024-01-14 01:55:15 +0900

    style: navbar 스타일 변경
    navbar 스타일 변경

--- dormitory-families-fe/src/components/common/NavBar.tsx
+++ dormitory-families-fe/src/components/common/NavBar.tsx
@@ -5,13 +5,13 @@ import { MENU_LIST } from '@/utils/nav';
 
 export default function NavBar() {
     return (
-        <div className="fixed bottom-0 flex justify-center items-center ">
-            <div className="bg-white flex rounded-t-[32px] py-3 px-5 justify-evenly gap-x-[30px] items-center drop-shadow-[0_-4px_12px_rgba(0,0,0,0.08)]">
+        <div className="fixed bottom-0 flex justify-center items-center">
+            <div className="bg-white flex rounded-t-[32px] gap-x-4 py-3 px-5 items-center drop-shadow-[0_-4px_12px_rgba(0,0,0,0.08)]">
                 {MENU_LIST.map((menu) => {
                     return (
-                        <Link href={menu.path} key={menu.id} className="h-[52px] text-[12px] flex flex-col justify-center items-center">
+                        <Link href={menu.path} key={menu.id} className="w-[54px] h-[52px] text-[12px] flex flex-col justify-center items-center">
                             <menu.Icon />
-                            <span className="text-h5 text-gray4">{menu.name}</span>
+                            <span className="text-h6 text-gray4">{menu.name}</span>
                         </Link>
                     );
                 })}
