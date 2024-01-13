91a2602dcdcf29525645ef706d0cb640c08323fc
rusia9217 <110519493+unnimm@users.noreply.github.com>
2024-01-14 00:30:34 +0900

    style: navbar fixed className 위치 변경
    navbar fixed className 위치 변경

--- dormitory-families-fe/src/components/common/NavBar.tsx
+++ dormitory-families-fe/src/components/common/NavBar.tsx
@@ -5,8 +5,8 @@ import { MENU_LIST } from '@/utils/nav';
 
 export default function NavBar() {
     return (
-        <div className="flex justify-center items-center ">
-            <div className="fixed bottom-0 bg-white flex rounded-t-[32px] py-3 px-5 justify-evenly gap-x-[30px] items-center drop-shadow-[0_-4px_12px_rgba(0,0,0,0.08)]">
+        <div className="fixed bottom-0 flex justify-center items-center ">
+            <div className="bg-white flex rounded-t-[32px] py-3 px-5 justify-evenly gap-x-[30px] items-center drop-shadow-[0_-4px_12px_rgba(0,0,0,0.08)]">
                 {MENU_LIST.map((menu) => {
                     return (
                         <Link href={menu.path} key={menu.id} className="h-[52px] text-[12px] flex flex-col justify-center items-center">
